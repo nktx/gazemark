60c7817bcfc809a4613f0227e9125f83190334a6
nktx <nkt.unknown@gmail.com>
2016-05-13 22:46:15 +0800

    feat: add result analysis

--- app/scripts/result.js
+++ app/scripts/result.js
@@ -16,25 +16,51 @@ $(function() {
 								.attr('class', 'result-svg');
 
 	function drawResult(result){
+
+		var errorTaskNum = 0;
+		var durationSum = 0;
+		var selectionSum = 0;
+		var taskNum = 48;
+
 		result.forEach(function (data){
-			
-			var translatedPath = data.path.map(function(p){
-				return {
-					X: p.X + offsetX - data.startPosition.X,
-					Y: p.Y + offsetY - data.startPosition.Y
-				}
-			});
+			if (data.subject == '') {
 
-			console.log(translatedPath);
+				durationSum += data.duration;
+				selectionSum += data.result.length;
 
-			svg.append('path')
-				.attr({
-					'd': line(translatedPath),
-					'stroke': '#333',
-					'stroke-width': '1px',
-					'fill': 'none'
+				var translatedPath = data.path.map(function(p){
+					return {
+						X: p.X + offsetX - data.startPosition.X,
+						Y: p.Y + offsetY - data.startPosition.Y
+					}
 				});
+
+				if ((data.result.length >= 1) && (data.result.slice(-1)[0] == data.task)) {
+					svg.append('path')
+					.attr({
+						'd': line(translatedPath),
+						'stroke': '#34495e',
+						'stroke-width': '1px',
+						'fill': 'none'
+					});
+				} else {
+					console.log(data);
+					errorTaskNum++;
+
+					svg.append('path')
+					.attr({
+						'd': line(translatedPath),
+						'stroke': '#e74c3c',
+						'stroke-width': '1px',
+						'fill': 'none'
+					});
+				}				
+			}
 		});
+
+		console.log("error rate (%): " + errorTaskNum*100/taskNum);
+		console.log("mean completion time (ms): " + durationSum/taskNum);
+		console.log("mean selections (/task): " + selectionSum/taskNum);
 	}
 
 	var offsetX = $(window).width()/2;
